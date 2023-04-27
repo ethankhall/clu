24841bd3a276efa2fab7960511eaad42aeb26f71
Ethan Hall <ethan.hall@gusto.com>
2023-04-27 10:13:40 -0700

    use version number

--- .github/bin/build-all.sh
+++ .github/bin/build-all.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+set -eux
+
+VERSION="${1:-'0.1.1-SNAPSHOT'}"
+
 mkdir -p target/artifacts
-cargo build --release --verbose --target aarch64-apple-darwin
-cargo build --release --verbose --target x86_64-apple-darwin
+cargo build --release --verbose --target aarch64-apple-darwin --config package.version="$VERSION"
+cargo build --release --verbose --target x86_64-apple-darwin --config package.version="$VERSION"
 
 lipo target/aarch64-apple-darwin/release/clu target/x86_64-apple-darwin/release/clu -create -output target/artifacts/clu
 
